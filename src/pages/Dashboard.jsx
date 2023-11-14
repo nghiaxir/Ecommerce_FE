d07f92adfd00f6c3ddc4197c41825cde091058ef
Dinh Trong Nghia <nghiadt@mirabo-global.com>
2023-11-14 22:30:47 +0700

    add chart js on dashboard

--- src/pages/Dashboard.jsx
+++ src/pages/Dashboard.jsx
@@ -1,11 +1,58 @@
 import React from 'react'
-import DashboardWrapper, { DashboardWrapperMain, DashboardWrapperRight } from '../components/DashBoardWrapper/DashBoardWrapper'
+import { Bar } from 'react-chartjs-2'
+import Box from '../components/box/Box'
 
+import DashboardWrapper, { DashboardWrapperMain, DashboardWrapperRight } from '../components/DashBoardWrapper/DashBoardWrapper'
+import data from '../constants/data'
+import colors from '../constants/color'
+import {
+    Chart as ChartJS,
+    CategoryScale,
+    LinearScale,
+    PointElement,
+    BarElement,
+    Title,
+    Tooltip,
+    Legend
+} from 'chart.js'
+import SummaryBox, { SummaryBoxSpecial } from '../components/Summary/SummaryBox'
+
+ChartJS.register(
+    CategoryScale,
+    LinearScale,
+    PointElement,
+    BarElement,
+    Title,
+    Tooltip,
+    Legend
+)
 const Dashboard = () => {
   return (
     <DashboardWrapper>
       <DashboardWrapperMain>
-        DashboardMain
+        <div className="row">
+            <div className="col-8 col-md-12">
+                <div className="row">
+                    {
+                        data.summary.map((item, index) => (
+                            <div key={`summary-${index}`} className="col-6 col-md-6 col-sm-12 mb">
+                                <SummaryBox item={item} />
+                            </div>
+                        ))
+                    }
+                </div>
+            </div>
+            <div className="col-4 hide-md">
+                <SummaryBoxSpecial item={data.revenueSummary} />
+            </div>
+        </div>
+        <div className="row">
+            <div className="col-12">
+                <Box>
+                    <RevenueByMonthsChart />
+                </Box>
+            </div>
+        </div>
       </DashboardWrapperMain>
       <DashboardWrapperRight>
         DashboardRight
@@ -15,3 +62,59 @@ const Dashboard = () => {
 }
 
 export default Dashboard
+
+const RevenueByMonthsChart = () => {
+    const chartOptions = {
+        responsive: true,
+        maintainAspectRatio: false,
+        scales: {
+            xAxes: {
+                grid: {
+                    display: false,
+                    drawBorder: false
+                }
+            },
+            yAxes: {
+                grid: {
+                    display: false,
+                    drawBorder: false
+                }
+            }
+        },
+        plugins: {
+            legend: {
+                display: false
+            },
+            title: {
+                display: false
+            }
+        },
+        elements: {
+            bar: {
+                backgroundColor: colors.orange,
+                borderRadius: 20,
+                borderSkipped: 'bottom'
+            }
+        }
+    }
+
+    const chartData = {
+        labels: data.revenueByMonths.labels,
+        datasets: [
+            {
+                label: 'Revenue',
+                data: data.revenueByMonths.data
+            }
+        ]
+    }
+    return (
+        <>
+            <div className="title mb">
+                Revenue by months
+            </div>
+            <div>
+                <Bar options={chartOptions} data={chartData} height={`300px`} />
+            </div>
+        </>
+    )
+}
